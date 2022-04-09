d87a4e412e2725895cf1640dfa2e67a25cb401ab
DEmanderbag <dusan-milosevic@outlook.com>
2022-04-09 14:13:18 +0200

    Update footer, translate and sizing. Add head data

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,19 +1,28 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import styles from "../../styles/layout/Footer.module.scss";
+import useTranslation from "next-translate/useTranslation";
 
 const Footer = () => {
   let router = useRouter();
+  let { t } = useTranslation("common");
   return (
-    <footer>
-      <ul>
-        {router.locales.map((locale) => (
-          <li key={locale}>
-            <Link href={router.asPath} locale={locale}>
-              <a>{locale}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <footer className={styles.footer}>
+      <div className={styles.wrapper}>
+        <p>
+          {t("createdBY")}{" "}
+          <a href="https://github.com/DEmanderbag"> DEmanderbag (Github)</a>
+        </p>
+        <ul className={styles.languages}>
+          {router.locales.map((locale) => (
+            <li key={locale}>
+              <Link href={router.asPath} locale={locale}>
+                <a>{locale}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </footer>
   );
 };
